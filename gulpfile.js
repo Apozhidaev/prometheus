7f578daa136b690586d5d4d7f9afdd0fc3e0455f
alexanderpo <al.po@outlook.com>
2016-10-03 21:42:42 +0300

    восстановил структуру проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 ﻿var gulp = require("gulp");
+var insert = require('gulp-insert');
+var handlebars = require('handlebars');
 var rimraf = require("rimraf");
 var fs = require('fs');
 
@@ -7,15 +9,54 @@ var paths = {
     src: './src/'
 };
 
-gulp.task('clean:www', function (cb) {
+handlebars.registerHelper('setTitle', function(options) {
+    options.data.root.title = options.fn(this);
+});
+
+handlebars.registerHelper('link', function(text, url, options) {
+    text = handlebars.Utils.escapeExpression(text);
+    url  = handlebars.Utils.escapeExpression(url);
+
+    var className = url.indexOf(options.data.root.file) !== -1 ? "selected" : "";
+    var result = '<a class="'+ className + '" href="' + url + '">' + text + '</a>';
+
+    return new handlebars.SafeString(result);
+});
+
+gulp.task('clean', function (cb) {
   rimraf(paths.wwwroot, cb);
 });
 
-gulp.task('copy:www', ['clean:www'], function () {
+gulp.task('copy', ['clean'], function () {
 
-    var all = [];
-
-    return gulp.src(all)
+    return gulp.src(['!' + paths.src + '**/*.{html,hbs}', paths.src + '**/*.*'])
         .pipe(gulp.dest(paths.wwwroot));
+
+});
+
+gulp.task('build', ['copy'], function () {
+
+    var source = fs.readFileSync(paths.src + 'layout.hbs', 'utf8');
+    var template = handlebars.compile(source);
+
+    gulp.src(paths.src + '*.html')
+        .pipe(insert.transform(function(contents, file) {
+            var context = {file: file.relative};
+            context.body = handlebars.compile(contents)(context);
+            return template(context);
+        }))
+        .pipe(gulp.dest(paths.wwwroot));
+
+    var sourceRu = fs.readFileSync(paths.src + 'ru/layout.hbs', 'utf8');
+    var templateRu = handlebars.compile(sourceRu);
+
+    gulp.src(paths.src + 'ru/*.html')
+        .pipe(insert.transform(function(contents, file) {
+            var context = {file: file.relative};
+            context.body = handlebars.compile(contents)(context);
+            return templateRu(context);
+        }))
+        .pipe(gulp.dest(paths.wwwroot + 'ru/'));
+
 
 });
