6d8cc32ecb3168b78f81e2a5348f96e80f8ab46a
alexanderpo <al.po@outlook.com>
2016-10-03 16:38:21 +0300

    hotfix

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+﻿var gulp = require("gulp");
+var rimraf = require("rimraf");
+var fs = require('fs');
+
+var paths = {
+    wwwroot: './wwwroot/',
+    src: './src/'
+};
+
+gulp.task('clean:www', function (cb) {
+  rimraf(paths.wwwroot, cb);
+});
+
+gulp.task('copy:www', ['clean:www'], function () {
+
+    var all = [];
+
+    return gulp.src(all)
+        .pipe(gulp.dest(paths.wwwroot));
+
+});
